dc8e47a0e7c3cad75a520aed843b2eecad087fcf
EmreOkyay <emreokyay33@gmail.com>
2025-10-23 23:31:29 +0300

    added config files

---
+++ src/main/java/org/example/httpserver/config/ConfigurationManager.java
@@ -0,0 +1,19 @@
+package org.example.httpserver.config;
+
+public class ConfigurationManager {
+
+    private static ConfigurationManager myConfigurationManager;
+
+    private ConfigurationManager() {
+    }
+
+    public static ConfigurationManager getInstance() {
+        if (myConfigurationManager == null)
+            myConfigurationManager = new ConfigurationManager();
+        return myConfigurationManager;
+    }
+
+    public void loadConfigurationFile(String filePath) {
+
+    }
+}
